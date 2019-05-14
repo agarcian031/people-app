cf73fb78a8c46cbd138d452285455d86f3cac34e
agarcian031 <algarcian@outlook.com>
2019-05-14 17:16:12 -0600

    Used Faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Loop faker gem data into Person db table 
+13.times do 
+  Person.create(
+    first_name: Faker::Movies::Hobbit.thorins_company,
+    last_name: "Dwarf", 
+    age: Faker::Number.number(2),
+    hair_color: Faker::Color.color_name, 
+    eye_color: Faker::Color.color_name, 
+    gender: "Male", 
+    alive: Faker::Boolean.boolean 
+  )
+  end 
